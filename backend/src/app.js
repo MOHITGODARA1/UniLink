46360e2f81cb333f6feb755be7afcb655d7dd30d
Mohit Godara <kukuchoudhary816@gmail.com>
2025-12-11 11:48:27 +0530

    add a google file

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 const app=express();
 app.use(express.json());
-
+app.use(express.static("public"));
 app.use(cors({
   origin: [
     "http://localhost:5173",
